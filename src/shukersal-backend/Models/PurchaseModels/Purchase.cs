619cdb8f3aba2f7dfa3f2d98e99d5c1958a33d17
ShovalYarkoni <shovaliy@post.bgu.ac.il>
2023-04-15 19:13:40 +0300

    purchase

--- src/shukersal-backend/Models/PurchaseModels/Purchase.cs
+++ src/shukersal-backend/Models/PurchaseModels/Purchase.cs
@@ -2,6 +2,14 @@
 {
     public class Purchase
     {
+        public Purchase(long memberId,Member member, DateTime purchaseDate)
+        {
+            MemberId = memberId;
+            Member = member;
+            PurchaseDate = purchaseDate;
+            PurchaseItems = new List<PurchaseItem>();
+        }
+
         // TODO: Connect billing/delivery service via invoice id / delivery id etc.
         public long Id { get; set; }
         public long MemberId { get; set; }
--- src/shukersal-backend/Models/PurchaseModels/PurchaseItem.cs
+++ src/shukersal-backend/Models/PurchaseModels/PurchaseItem.cs
@@ -2,6 +2,20 @@
 {
     public class PurchaseItem
     {
+        private long id1;
+        private long id2;
+        private double price;
+
+        public PurchaseItem(long id1, Purchase purchase, long id2, Product product, int quantity, double price)
+        {
+            this.id1 = id1;
+            Purchase = purchase;
+            this.id2 = id2;
+            Product = product;
+            Quantity = quantity;
+            this.price = price;
+        }
+
         // TODO: Check if needed to connect discounts applied 
         public long Id { get; set; }
         public long PurchaseId { get; set; }
