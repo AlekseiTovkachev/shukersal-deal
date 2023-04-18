9956f28ea725185ed995b246b3796940325e64d5
AlekseiTovkachev <atovkachev@gmail.com>
2023-04-18 20:55:50 +0300

    Divided the store controller into two classes Added response utility class

--- src/shukersal-backend/Models/StoreModels/StorePost.cs
+++ src/shukersal-backend/Models/StoreModels/StorePost.cs
@@ -4,7 +4,7 @@ namespace shukersal_backend.Models
 {
     public class StorePost
     {
-        public long Id { get; set; }
+        //public long Id { get; set; }
         [Required]
         public string Name { get; set; }
         public string Description { get; set; }
---
+++ src/shukersal-backend/Utility/Response.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Net;
+
+namespace shukersal_backend.Utility
+{
+    public class Response<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T? Result { get; set; }
+        public string? ErrorMessage { get; set; }
+
+        public HttpStatusCode StatusCode { get; set; }
+
+        public static Response<T> Success(HttpStatusCode statusCode, T result)
+        {
+            return new Response<T>
+            {
+                IsSuccess = true,
+                StatusCode = statusCode,
+                Result = result
+            };
+        }
+
+        public static Response<T> Error(HttpStatusCode statusCode, string errorMessage)
+        {
+            return new Response<T>
+            {
+                IsSuccess = false,
+                StatusCode = statusCode,
+                ErrorMessage = errorMessage
+            };
+        }
+    }
+}
