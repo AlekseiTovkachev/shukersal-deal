f77793bd2bfd5060a1f4a8d6a3333738e110a978
ShovalYarkoni <shovaliy@post.bgu.ac.il>
2023-04-22 13:33:12 +0300

    started tests

---
+++ src/shukersal-backend.Tests/ServiceTests/PurchaseServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using shukersal_backend.Domain;
+using shukersal_backend.Models;
+using shukersal_backend.Models.ShoppingCartModels;
+using shukersal_backend.Utility;
+using System.Net;
+using Xunit.Abstractions;
+
+
+namespace shukersal_backend.Tests.ServiceTests
+{
+    internal class PurchaseServiceTests
+    {
+        private readonly PurchaseService _purchaseService;
+        private readonly Mock<PurchaseContext> _purchaseContextMock;
+        private readonly Mock<StoreContext> _storeContextMock;
+        private readonly Mock<MemberContext> _memberContextMock;
+        private readonly Mock<ShoppingCartContext> _shoppingCartContextMock;
+        private readonly ITestOutputHelper _output;
+
+        public PurchaseServiceTests(ITestOutputHelper output)
+        {
+            _purchaseContextMock= new Mock<PurchaseContext>();
+            _storeContextMock = new Mock<StoreContext>();
+            _memberContextMock = new Mock<MemberContext>();
+            _shoppingCartContextMock = new Mock<ShoppingCartContext>();
+            _output = output;
+            _purchaseService = new PurchaseService(_purchaseContextMock.Object, _storeContextMock.Object,_memberContextMock.Object,_shoppingCartContextMock.Object);
+        }
+    }
+}
