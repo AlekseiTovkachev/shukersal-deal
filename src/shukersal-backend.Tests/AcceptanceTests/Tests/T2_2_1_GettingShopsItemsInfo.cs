3dde4d2f23d8701b6bfc6abd705ad325080e69cc
yuvalsegevv <syuva@post.bgu.ac.il>
2023-05-04 00:21:21 +0300

    written some 2.1.X and 2.2.X AT

--- src/shukersal-backend.Tests/AcceptanceTests/Tests/T2_2_1_GettingShopsItemsInfo.cs
+++ src/shukersal-backend.Tests/AcceptanceTests/Tests/T2_2_1_GettingShopsItemsInfo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using shukersal_backend.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +8,30 @@ using Xunit.Abstractions;
 
 namespace shukersal_backend.Tests.AcceptanceTests
 {
-    internal class T2_2_1_GettingShopsItemsInfo : AcceptanceTest
+    public class T2_2_1_GettingShopsItemsInfo : AcceptanceTest
     {
-        public T2_2_1_GettingShopsItemsInfo(ITestOutputHelper output) : base(output) { }
+        public T2_2_1_GettingShopsItemsInfo(ITestOutputHelper output) : base(output) {
+            bridge.Register(new Models.RegisterPost { Id = 2, Username = "testUsername", Password = "testPassword" });
+            bridge.Login(new Models.LoginPost { Username = "testUsername", Password = "testPassword" });
+        }
+        [Fact]
+        public async void GetShopInfo()
+        {
+            var res = bridge.GetStores();
+            res.Wait();
+            var res2 = bridge.Register(new RegisterPost { Id = 11, Username = "User", Password = "UserPass2" });
+            res2.Wait();
+            Assert.False(res2.Result is ActionResult);
+        }
+        [Fact]
+        public async void TestRegisterSameUsername()
+        {
+            var res = bridge.Register(new RegisterPost { Id = 10, Username = "User", Password = "UserPass" });
+            res.Wait();
+            var res2 = bridge.Register(new RegisterPost { Id = 11, Username = "User", Password = "UserPass2" });
+            res2.Wait();
+            Assert.False(res2.Result is ActionResult);
+        }
     }
+
 }
--- src/shukersal-backend.Tests/AcceptanceTests/Tests/T2_2_4_RemoveItem.cs
+++ src/shukersal-backend.Tests/AcceptanceTests/Tests/T2_2_4_RemoveItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using shukersal_backend.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +8,40 @@ using Xunit.Abstractions;
 
 namespace shukersal_backend.Tests.AcceptanceTests
 {
-    internal class T2_2_4_RemoveItem : AcceptanceTest
+    public class T2_2_4_RemoveItem : AcceptanceTest
     {
-        public T2_2_4_RemoveItem(ITestOutputHelper output) : base(output) { }
+        public T2_2_4_RemoveItem(ITestOutputHelper output) : base(output)
+        {
+            bridge.Register(new Models.RegisterPost { Id = 2, Username = "testUsername", Password = "testPassword" });
+            bridge.Login(new Models.LoginPost { Username = "testUsername", Password = "testPassword" });
+        }
+        [Fact]
+        public void RemoveItemPositive()
+        {
+            var product = bridge.GetStoreProducts(1).Result.Value.Products.ElementAt(1);
+            bridge.AddItemToCart(1, new Models.ShoppingItem { Id = 100, Product = product, Quantity = 1 }).Wait();
+            var res = bridge.RemoveItemFromCart(1, 100);
+            res.Wait();
+            Assert.True(res.Result is IActionResult);
+        }
+        [Fact]
+        public void RemoveItemDoesntExist()
+        {
+            var product = bridge.GetStoreProducts(1).Result.Value.Products.ElementAt(1);
+            var res = bridge.RemoveItemFromCart(1, 100);
+            res.Wait();
+            Assert.False(res.Result is IActionResult);
+        }
+        [Fact]
+        public void RemoveItemDoubleRequest()
+        {
+            var product = bridge.GetStoreProducts(1).Result.Value.Products.ElementAt(1);
+            bridge.AddItemToCart(1, new Models.ShoppingItem { Id = 100, Product = product, Quantity = 1 }).Wait();
+            var res1 = bridge.RemoveItemFromCart(1, 100);
+            var res2 = bridge.RemoveItemFromCart(1, 100);
+            res1.Wait();
+            res2.Wait();
+            Assert.True(res1.Result is IActionResult ^ res2.Result is IActionResult);
+        }
     }
 }
