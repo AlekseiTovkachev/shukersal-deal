66244a89577b5e7f7f4bf9c36f202a3fba970a9e
yonatanShnits <shnits@post.bgu.ac.il>
2023-05-06 14:18:20 +0300

    added LoginResponse

--- src/shukersal-backend/ServiceLayer/AuthService.cs
+++ src/shukersal-backend/ServiceLayer/AuthService.cs
@@ -9,6 +9,7 @@ using Microsoft.IdentityModel.Tokens;
 using NuGet.Protocol.Plugins;
 using shukersal_backend.DomainLayer.Controllers;
 using shukersal_backend.Models;
+using shukersal_backend.Models.MemberModels;
 using shukersal_backend.Utility;
 using System.Data;
 using System.IdentityModel.Tokens.Jwt;
@@ -35,7 +36,7 @@ namespace shukersal_backend.ServiceLayer
 
         [HttpPost]
         [Route("Login")]
-        public async Task<ActionResult> Login(LoginPost loginRequest, IConfiguration _configuration)
+        public async Task<ActionResult<LoginResponse>> Login(LoginPost loginRequest, IConfiguration _configuration)
         {
             if (ModelState.IsValid)
             {
@@ -44,8 +45,8 @@ namespace shukersal_backend.ServiceLayer
                 {
                     return BadRequest(ModelState);
                 }
-                var tokenString = response.Result;
-                return Ok(tokenString);
+                var loginResponse = response.Result;
+                return Ok(loginResponse);
             }
             else
             {
