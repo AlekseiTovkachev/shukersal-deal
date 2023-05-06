7277dd716dd17a509dc366c8158c88c202bb1c89
ShovalYarkoni <shovaliy@post.bgu.ac.il>
2023-05-06 19:23:47 +0300

    commit

--- src/shukersal-backend.Tests/AcceptanceTests/Tests/T2_2_5_Purchase.cs
+++ src/shukersal-backend.Tests/AcceptanceTests/Tests/T2_2_5_Purchase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using shukersal_backend.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +8,20 @@ using Xunit.Abstractions;
 
 namespace shukersal_backend.Tests.AcceptanceTests
 {
-    internal class T2_2_5_Transaction : AcceptanceTest
+    public class T2_2_5_Transaction : AcceptanceTest
     {
-        public T2_2_5_Transaction(ITestOutputHelper output) : base(output) { }
+        public T2_2_5_Transaction(ITestOutputHelper output) : base(output) {
+            
+        }
+
+        [Fact]
+        public async void PurchaseAShoppingCart_Member_Valid()
+        {
+            var registerResp = await bridge.Register(new RegisterPost { Username = "testUsername8", Password = "testPassword" });
+            var loginResp =await bridge.Login(new LoginPost { Username = "testUsername8", Password = "testPassword" });
+            var storeResp = await bridge.CreateStore(new StorePost { Name = "mystore", Description = "mystoredesc", RootManagerMemberId = 1 });
+            //var priduct = await bridge.AddProduct(storeResp.Value.Id, new ProductPost {Name="myproduct1" })
+        }
+
     }
 }
