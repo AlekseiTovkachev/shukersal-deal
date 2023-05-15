dfa3425db906d953afa0c4f0f25ebd54be5e6d2f
yonatanShnits <shnits@post.bgu.ac.il>
2023-05-15 19:42:39 +0300

    added transaction tests

--- src/shukersal-backend.Tests/AcceptanceTests/Tests/T2_2_5_Purchase.cs
+++ src/shukersal-backend.Tests/AcceptanceTests/Tests/T2_2_5_Purchase.cs
@@ -4,6 +4,7 @@ using shukersal_backend.Models.PurchaseModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit.Abstractions;
@@ -16,8 +17,10 @@ namespace shukersal_backend.Tests.AcceptanceTests
         private List<PaymentDetails> billingDetails;
         private List<DeliveryDetails> deliveryDetails;
 
+
         public T2_2_5_Transaction(ITestOutputHelper output) : base(output) {
             TransactionAT_init(); 
+            //using the init data from the bridge
         }
 
         private void TransactionAT_init()
@@ -41,6 +44,15 @@ namespace shukersal_backend.Tests.AcceptanceTests
                          CardNumber = "1111111111111111",
                          ExpirationDate = new DateOnly(),
                          CVC = "123",
+                     },
+                     new PaymentDetails
+                    {
+                         HolderFirstName = "bad_payment",
+                         HolderLastName = "bad_payment",
+                         HolderID = "bad_payment",
+                         CardNumber = "bad_payment",
+                         ExpirationDate = new DateOnly(),
+                         CVC = "000",
                      }
             };
 
@@ -61,6 +73,14 @@ namespace shukersal_backend.Tests.AcceptanceTests
                     ReceiverPhoneNum="0500000000",
                     ReceiverAddress= "ReceiverAddress",
                     ReceiverPostalCode="1234567"
+                },
+                new DeliveryDetails
+                {
+                    ReceiverFirstName= "bad_delivery",
+                    ReceiverLastName= "bad_delivery",
+                    ReceiverPhoneNum="bad_delivery",
+                    ReceiverAddress= "bad_delivery",
+                    ReceiverPostalCode="bad_delivery"
                 }
             };
         }
@@ -68,25 +88,92 @@ namespace shukersal_backend.Tests.AcceptanceTests
         [Fact]
         public async void PurchaseAShoppingCart_Member_Valid()
         {
-            var registerResp = await bridge.Register(new RegisterPost {Username = "testUsername8", Password = "testPassword" });
-            var loginResp = await bridge.Login(new LoginPost { Username = "testUsername8", Password = "testPassword" });
             var storeResp = await bridge.CreateStore(new StorePost { Name = "mystore", Description = "mystoredesc", RootManagerMemberId = 2 });
             var product = await bridge.AddProduct(storeResp.Value.Id, new ProductPost { Name = "myproduct1", Description = "myproduct1Desc", Price = 10, UnitsInStock = 3, IsListed = true, CategoryId = 1 });
             await bridge.AddItemToCart(1, new ShoppingItem { Product = product as Product, Quantity = 1 });
             var purchaseResult = await bridge.PurchaseAShoppingCart(
-            new TransactionPost {MemberId = 1,BillingDetails = billingDetails.ElementAt(0),DeliveryDetails = deliveryDetails.ElementAt(0),TransactionItems=new List<TransactionItem> { 
-            new TransactionItem { ProductId = ((Product)product).Id,StoreId = storeResp.Value.Id,ProductName = "myproduct1",ProductDescription = "myproduct1Desc",FullPrice = 10 } } });
+            new TransactionPost
+            {
+                MemberId = 1,
+                BillingDetails = billingDetails.ElementAt(0),
+                DeliveryDetails = deliveryDetails.ElementAt(0),
+                TransactionItems = new List<TransactionItem> {
+            new TransactionItem { ProductId = ((Product)product).Id,StoreId = storeResp.Value.Id,ProductName = "myproduct1",ProductDescription = "myproduct1Desc",FullPrice = 10 } }
+            });
             Assert.NotNull(purchaseResult.Value);
-            var history= await bridge.BrowseTransactionHistory(1);
+            var history = await bridge.BrowseTransactionHistory(1);
             Assert.NotNull(purchaseResult.Value);
-            Assert.Equal(1,history.Value.TransactionItems.Count);
-            var products= await bridge.GetStoreProducts(storeResp.Value.Id);
+            Assert.Equal(1, history.Value.TransactionItems.Count);
+            var products = await bridge.GetStoreProducts(storeResp.Value.Id);
             var updated = products.Value.Products.ElementAt(0);
-            Assert.Equal(2,updated.UnitsInStock);
+            Assert.Equal(2, updated.UnitsInStock);
             var shoppingCart = bridge.GetShoppingCartByUserId(2).Result.Value;
             Assert.Equal(0, shoppingCart.ShoppingBaskets.Count);
-
         }
 
+
+        public async void PurchaseAShoppingCart_member_not_enough_in_stock()
+        {
+            var storeResp = await bridge.GetStore(1);
+            var product = await bridge.AddProduct(storeResp.Value.Id, new ProductPost { Name = "myproduct2", Description = "myproduct2Desc", Price = 10, UnitsInStock = 0, IsListed = true, CategoryId = 1 });
+            await bridge.AddItemToCart(1, new ShoppingItem { Product = product as Product, Quantity = 1 });
+            var purchaseResult = await bridge.PurchaseAShoppingCart(
+            new TransactionPost
+            {
+                MemberId = 1, //not right user id
+                BillingDetails = billingDetails.ElementAt(0),
+                DeliveryDetails = deliveryDetails.ElementAt(0),
+                TransactionItems = new List<TransactionItem> {
+            new TransactionItem { ProductId = ((Product)product).Id,StoreId = storeResp.Value.Id,ProductName = "myproduct1",ProductDescription = "myproduct1Desc",FullPrice = 10 } }
+            });
+            Assert.NotNull(purchaseResult.Value);
+            Assert.IsType<BadRequestObjectResult>(purchaseResult.Result);
+            var resultValue = (BadRequestObjectResult)purchaseResult.Result;
+            Assert.Equal((int)HttpStatusCode.BadRequest, resultValue.StatusCode);
+            Assert.Equal("", resultValue.Value);
+        }
+
+        public async void PurchaseAShoppingCart_bad_delivery()
+        {
+            var storeResp = await bridge.GetStore(1);
+            var product = await bridge.AddProduct(storeResp.Value.Id, new ProductPost { Name = "myproduct2", Description = "myproduct2Desc", Price = 10, UnitsInStock = 0, IsListed = true, CategoryId = 1 });
+            await bridge.AddItemToCart(1, new ShoppingItem { Product = product as Product, Quantity = 1 });
+            var purchaseResult = await bridge.PurchaseAShoppingCart(
+            new TransactionPost
+            {
+                MemberId = 1, 
+                BillingDetails = billingDetails.ElementAt(0),
+                DeliveryDetails = deliveryDetails.ElementAt(2),//Bad delivery see at the delivery list.
+                TransactionItems = new List<TransactionItem> {
+            new TransactionItem { ProductId = ((Product)product).Id,StoreId = storeResp.Value.Id,ProductName = "myproduct1",ProductDescription = "myproduct1Desc",FullPrice = 10 } }
+            });
+            Assert.NotNull(purchaseResult.Value);
+            Assert.IsType<BadRequestObjectResult>(purchaseResult.Result);
+            var resultValue = (BadRequestObjectResult)purchaseResult.Result;
+            Assert.Equal((int)HttpStatusCode.BadRequest, resultValue.StatusCode);
+            Assert.Equal("delivery Service Confirmation failed", resultValue.Value);
+        }
+
+
+        public async void PurchaseAShoppingCart_bad_payment()
+        {
+            var storeResp = await bridge.GetStore(1);
+            var product = await bridge.AddProduct(storeResp.Value.Id, new ProductPost { Name = "myproduct3", Description = "myproduct2Desc", Price = 10, UnitsInStock = 0, IsListed = true, CategoryId = 1 });
+            await bridge.AddItemToCart(1, new ShoppingItem { Product = product as Product, Quantity = 1 });
+            var purchaseResult = await bridge.PurchaseAShoppingCart(
+            new TransactionPost
+            {
+                MemberId = 1, 
+                BillingDetails = billingDetails.ElementAt(2),//Bad payment see at the payent list.
+                DeliveryDetails = deliveryDetails.ElementAt(0),
+                TransactionItems = new List<TransactionItem> {
+            new TransactionItem { ProductId = ((Product)product).Id,StoreId = storeResp.Value.Id,ProductName = "myproduct1",ProductDescription = "myproduct1Desc",FullPrice = 10 } }
+            });
+            Assert.NotNull(purchaseResult.Value);
+            Assert.IsType<BadRequestObjectResult>(purchaseResult.Result);
+            var resultValue = (BadRequestObjectResult)purchaseResult.Result;
+            Assert.Equal((int)HttpStatusCode.BadRequest, resultValue.StatusCode);
+            Assert.Equal("Payment Service Confirmation failed", resultValue.Value);
+        }
     }
 }
