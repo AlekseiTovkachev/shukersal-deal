9d7718f7706812e338554e75059c65c9eb7b9063
AlekseiTovkachev <atovkachev@gmail.com>
2023-05-27 22:11:42 +0300

    Scenarios in progress
    adding scenarios

---
+++ src/shukersal-backend/ServiceLayer/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace shukersal_backend.ServiceLayer
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
